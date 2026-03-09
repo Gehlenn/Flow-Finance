bf7e670e578ec114215d119a041faaa4345d91d8
Gehlenn <danielgehlenn@gmail.com>
2026-03-09 02:57:59 -0300

    fix: allow X-Client-Version and X-Client-Platform headers in CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -90,7 +90,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Client-Version', 'X-Client-Platform']
 };
 app.use(cors(corsOptions));
 
